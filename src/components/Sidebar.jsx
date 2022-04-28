b673b76a91d1de80d1664b86bd4238ae41faf5f0
ernestorl239@gmail.com <ernestorl239@gmail.com>
2022-04-27 19:13:42 -0500

    Sidebar Functions and Design

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,42 +2,61 @@ import React from 'react'
 import Inicio from '../img/INICIO.png'
 import estadisticas from '../img/ESTADISTICAS.png'
 import logout from '../img/LOGOUT.png'
+import {BiLogOut} from 'react-icons/bi'
+import { IconContext } from "react-icons"
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineBarChart } from 'react-icons/ai'
 
-const Sidebar = () => {
+const Sidebar = ({setLogin, setEstadistics, setHome}) => {
+    const Home = () => {
+        setLogin(false);
+        setEstadistics(false);
+        setHome(true);
+    }
+
+    const Estadistics = () => {
+        setLogin(false);
+        setEstadistics(true);
+        setHome(false);
+    }
+
+    const Logout = () => {
+        setLogin(true);
+        setEstadistics(false);
+        setHome(false);
+    }
     return(
-        <div class="absolute top-0 flex flex-col place-content-between bg-black container w-max min-h-screen">
+        <div class="absolute top-0 left-0 flex flex-col place-content-between items-end bg-black w-24 min-h-screen text-white">
             <div>
-                <button>
-                    <img
-                    class="object-contain"
-                    src={Inicio}
-                    width="80"
-                    height="80"
-                    />
-                </button>
+                <div class='w-20 rounded-l-lg  transition ease-in-out duration-700 hover:bg-white hover:text-black'>
+                    <button onClick={() => Home()}class='left-4'>
+                    <IconContext.Provider value={{size:60}}>
+                        <AiOutlineHome/>
+                    </IconContext.Provider>
+                    </button>
+                </div>
+                
                 <br/>
-                <button>
-                    <img
-                    class="object-contain"
-                    src={estadisticas}
-                    width="80"
-                    height="80"
-                    />
-                </button>
+                <div class='w-20 rounded-l-lg  transition ease-in-out duration-700 hover:bg-white hover:text-black'>
+                    <button onClick={() => Estadistics()}>
+                    <IconContext.Provider value={{size:60}}>
+                        <AiOutlineBarChart/>
+                    </IconContext.Provider>
+                    </button>
+                </div>
+                
             </div>
         
             <br/>
-            <button>
-                <img
-                class="object-contain"
-                src={logout}
-                width="70"
-                height="70"
-                />
-            </button>
-        
+            <div class='w-20 rounded-l-lg  transition ease-in-out duration-700 hover:bg-white hover:text-black'>
+                <button onClick={() => Logout()}>
+                    <IconContext.Provider value={{size:60}}>
+                        <BiLogOut/>
+                    </IconContext.Provider>
+                </button>
+            </div>
             
-            
+
         </div>
     )
 }
