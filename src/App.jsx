70046c236fa9b3b0fc70d3bf251feca9acdb6e89
ernestorl239@gmail.com <ernestorl239@gmail.com>
2022-05-05 15:43:50 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ import UpdatePatient from "./components/UpdatePatient"
 
 function App() {
     //States patra el control del flujo
-    const [evalauaciones, setEvaluaciones] = useState(false);
-    const [logIn, setLogIn] = useState(true);
+    const [evalauaciones, setEvaluaciones] = useState(true);
+    const [logIn, setLogIn] = useState(false);
     const [recPassword, setRecPassword] = useState(false);
     const [regGeriatra, setRegGeriatra] = useState(false);
     const [validation, setValidation] = useState(false);
